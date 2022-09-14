3f6da7482389e62ea82baeafac653de3f4f44456
asankasub <asanka_sub@hotmail.com>
2022-09-14 15:49:50 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import smtplib
+import datetime as dt
+import random
+import pandas as pd
+
+##################### Extra Hard Starting Project ######################
+now = dt.datetime.now()
+month = now.month
+year = now.year
+day = now.day
+df = pd.read_csv("C:/Users/Asanka/Desktop/Python Practice/Birthday Wisher (Day 32) start/birthdays.csv")
+for index, row in df.iterrows():
+    if row["year"] == year and row["month"] == month and row["day"] == day:
+        name = row["name"]
+        email = row["email"]        
+        
+letters = ['C:/Users/Asanka/Desktop/Python Practice/Birthday Wisher (Day 32) start/letter_templates/letter_1.txt',
+'C:/Users/Asanka/Desktop/Python Practice/Birthday Wisher (Day 32) start/letter_templates/letter_2.txt',
+'C:/Users/Asanka/Desktop/Python Practice/Birthday Wisher (Day 32) start/letter_templates/letter_3.txt']
+chosen_letter = random.choice(letters)
+with open(chosen_letter, 'r') as file:
+    letter = file.read()
+    letter = letter.replace("[NAME]", name)
+with open(chosen_letter, 'w') as file:
+    file.write(letter)
+
+my_email = "alphwoodstock@gmail.com" 
+my_pass = "quebxagjxuawjoqm"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(my_email, my_pass)
+    connection.sendmail(from_addr=my_email, to_addrs=email, msg=f"Subject: Happy Birthday!\n\n{letter}")
+
+print(chosen_letter)
+
+
+
+
+/home/asankasub/letter_templates/letter_1.txt
+
+
+/home/asankasub/birthdays.csv
+
+
+/home/asankasub/main.py
---
+++ save.py
@@ -0,0 +1,12 @@
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+my_email = "alphwoodstock@gmail.com" 
+my_pass = "quebxagjxuawjoqm"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(my_email, my_pass)
+    connection.sendmail(from_addr=my_email, to_adrss=email, msg=f"Subject: Happy Birthday!\n\n{letter}")
